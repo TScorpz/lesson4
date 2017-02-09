284ebdea9140e68f0c11b1507870ef28a0397f78
TScorpz <shamaner88@gmail.com>
2017-02-09 23:24:43 +0200

    Html css js changes
    Minor html and style changes.
    Added divs inside ajax.html that are colored to different colors from
    the palette.

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,17 @@ $(document).ready(function() {
   });
   $(".localFileData").click(function() {
     $.get("readme.txt", function(response) {
-      console.log(response);
       $(".content").html(response);
     });
   });
   $(".externalData").click(function(){
     $.getJSON("http://www.colourlovers.com/api/palettes/random?jsonCallback=?",
-      { numResults: 1 },
-          function(colorPalettes) {
-          $("body").css("background-color", "#" + colorPalettes[0].colors[0]);
+      { numResults: 2 },
+      function(colorPalettes) {
+        $("body").css("background-color", "#" + colorPalettes[0].colors[0]);  //change body background color
+        $('.boxes div').each(function(index, element) {
+          $(this).css('background-color', '#' + colorPalettes[0].colors[++index]);  //change each div's background color
+        });
       }
     );
   });
